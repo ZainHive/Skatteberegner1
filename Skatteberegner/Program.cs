159726c178f0e8fa420a20ccf8a86ede4b3e8952
ZainHive <76405605+ZainHive@users.noreply.github.com>
2024-10-05 22:49:27 +0200

    Ny skatteberegner

--- Skatteberegner/Program.cs
+++ Skatteberegner/Program.cs
@@ -1,10 +1,29 @@
-﻿    public class Beregning {
-        static void Main(string[] args) {
+﻿using System;
 
-        }
-        
-        // Funktionen returnerer det beløb, der skal beskattes (altså ikke selve skatten).
-        public double SkatVedJulegave(double julegave, double andenGave) {
+public class Beregning
+{
+    private const decimal SkattefriGrænse = 1200m; // Skattefri grænse for gaver i 2021
+    private const decimal JulegaveGrænse = 900m; // Skattefri grænse for julegaver
+
+    public double SkatVedJulegave(decimal julegave, decimal andreGaver)
+    {
+        decimal samletGavebeløb = julegave + andreGaver;
+
+        if (samletGavebeløb <= SkattefriGrænse)
+        {
             return 0;
         }
+        else
+        {
+            decimal skattepligtigtBeløb = samletGavebeløb - SkattefriGrænse;
+            if (julegave <= JulegaveGrænse)
+            {
+                skattepligtigtBeløb -= julegave;
+            }
+            return (double)skattepligtigtBeløb;
+        }
     }
+
+    public static void Main(string[] args)
+    {
+        Beregning beregning = new Beregning();
